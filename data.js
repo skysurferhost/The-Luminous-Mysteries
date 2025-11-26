f37fc1f7fec8df089c906b0b90d055179a98143a
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2025-11-26 01:26:24 -0500

    update

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-the-baptism-of-the-lord",
-      "name": "The Baptism of the Lord",
+      "id": "0-1-the-baptism-of-the-lord",
+      "name": "1. The Baptism of the Lord",
       "levels": [
         {
           "tileSize": 256,
@@ -28,30 +28,30 @@ var APP_DATA = {
       ],
       "faceSize": 4096,
       "initialViewParameters": {
-        "yaw": 0.000024339300006559483,
-        "pitch": 0,
+        "yaw": 0.10920235673344969,
+        "pitch": -0.04628540847028795,
         "fov": 1.4134061960355204
       },
       "linkHotspots": [
         {
-          "yaw": 0.8549153822366922,
-          "pitch": 0.11061234348508364,
+          "yaw": 0.8519184659201429,
+          "pitch": 0.1191463543784046,
           "rotation": 0,
-          "target": "1-the-wedding-feast-at-canna"
+          "target": "1-2-the-wedding-feast-at-canna"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 0.8432667621044523,
-          "pitch": -0.296672168660848,
-          "title": "<span style=\"font-size: 13px; background-color: rgba(58, 68, 84, 0.8);\">The Baptism of Jesus&nbsp;</span>",
+          "yaw": 0.8223163948861192,
+          "pitch": -0.27377477993393917,
+          "title": "The Baptism of Jesus",
           "text": "Then Jesus came from Galilee to the Jordan to be baptized by John. But John tried to deter him, saying, “I need to be baptized by you, and do you come to me?” 15 Jesus replied, “Let it be so now; it is proper for us to do this to fulfill all righteousness.” Then John consented. 16 As soon as Jesus was baptized, he went up out of the water. At that moment heaven was opened, and he saw the Spirit of God descending like a dove and alighting on him. 17 And a voice from heaven said, “This is my Son, whom I love; with him I am well pleased.” (Matthew 3:13–17)"
         }
       ]
     },
     {
-      "id": "1-the-wedding-feast-at-canna",
-      "name": "The Wedding Feast at Canna",
+      "id": "1-2-the-wedding-feast-at-canna",
+      "name": "2. The Wedding Feast at Canna",
       "levels": [
         {
           "tileSize": 256,
@@ -83,30 +83,24 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 2.959611766112314,
-          "pitch": -0.030145689083878935,
+          "yaw": 2.948568646156973,
+          "pitch": 0.03310474120495854,
           "rotation": 0,
-          "target": "2-the-proclamation-of-the-kingdom"
-        },
-        {
-          "yaw": -1.7856730614706713,
-          "pitch": 0.06662837485324502,
-          "rotation": 0,
-          "target": "0-the-baptism-of-the-lord"
+          "target": "2-3-the-proclamation-of-the-kingdom"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 2.922054075157691,
-          "pitch": -0.3705952366140419,
+          "yaw": 2.871021549299231,
+          "pitch": -0.29438588691595413,
           "title": "Jesus Changes Water Into Wine",
           "text": "On the third day a wedding took place at Cana in Galilee. Jesus’ mother was there, and Jesus and his disciples had also been invited to the wedding. When the wine was gone, Jesus’ mother said to him, “They have no more wine.” “Woman, why do you involve me?” Jesus replied. “My hour has not yet come.” His mother said to the servants, “Do whatever he tells you.” Nearby stood six stone water jars, the kind used by the Jews for ceremonial washing, each holding from twenty to thirty gallons. Jesus said to the servants, “Fill the jars with water”; so they filled them to the brim. Then he told them, “Now draw some out and take it to the master of the banquet.” They did so, and the master of the banquet tasted the water that had been turned into wine. He did not realize where it had come from, though the servants who had drawn the water knew. Then he called the bridegroom aside and said, “Everyone brings out the choice wine first and then the cheaper wine after the guests have had too much to drink; but you have saved the best till now.” What Jesus did here in Cana of Galilee was the first of the signs through which he revealed his glory; and his disciples believed in him. (John 2:1-11)"
         }
       ]
     },
     {
-      "id": "2-the-proclamation-of-the-kingdom",
-      "name": "The Proclamation of The Kingdom",
+      "id": "2-3-the-proclamation-of-the-kingdom",
+      "name": "3. The Proclamation of The Kingdom",
       "levels": [
         {
           "tileSize": 256,
@@ -138,24 +132,73 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.451469258001941,
-          "pitch": 0.11188962560742155,
+          "yaw": -0.6559341124152844,
+          "pitch": -0.20717294456676072,
           "rotation": 0,
-          "target": "1-the-wedding-feast-at-canna"
+          "target": "3-4-the-transfiguration-of-the-lord"
         },
         {
-          "yaw": -0.5754484819804588,
-          "pitch": -0.2037510964289062,
+          "yaw": -2.3994409754128263,
+          "pitch": 0.09754640746898069,
           "rotation": 0,
-          "target": "0-the-baptism-of-the-lord"
+          "target": "1-2-the-wedding-feast-at-canna"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -0.5914883429330899,
-          "pitch": -0.40641488696023487,
+          "yaw": -0.7070247476008227,
+          "pitch": -0.4702268484140788,
           "title": "Jesus Announces the Good News",
           "text": "After John was put in prison, Jesus went into Galilee, proclaiming the good news of God. “The time has come,\" he said. “The kingdom of God has come near. Repent and believe the good news!” (Mark 1:14-15)"
+        }
+      ]
+    },
+    {
+      "id": "3-4-the-transfiguration-of-the-lord",
+      "name": "4. The Transfiguration of the Lord",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9247006390352386,
+          "pitch": 0.09638344719362557,
+          "rotation": 0,
+          "target": "2-3-the-proclamation-of-the-kingdom"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.472997734622714,
+          "pitch": -0.19520493217832957,
+          "title": "The Transfiguration",
+          "text": "After six days Jesus took with him Peter, James and John the brother of James, and led them up a high mountain by themselves. There he was transfigured before them. His face shone like the sun, and his clothes became as white as the light. Just then there appeared before them Moses and Elijah, talking with Jesus. Peter said to Jesus, “Lord, it is good for us to be here. If you wish, I will put up three shelters—one for you, one for Moses and one for Elijah.” While he was still speaking, a bright cloud covered them, and a voice from the cloud said, “This is my Son, whom I love; with him I am well pleased. Listen to him!” When the disciples heard this, they fell facedown to the ground, terrified. But Jesus came and touched them. “Get up,” he said. “Don’t be afraid.” When they looked up, they saw no one except Jesus. (Matthew 17:1-8)"
         }
       ]
     }
