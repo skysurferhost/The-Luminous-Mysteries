cac6846f3c3a4732bbf8dc23e2327349e05a8d09
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-02-05 01:48:24 -0500

    Update data.js

--- data.js
+++ data.js
@@ -45,7 +45,7 @@ var APP_DATA = {
           "yaw": 0.6319923911907424,
           "pitch": -0.17290606185455815,
           "title": "The Baptism of Jesus",
-          "text": "Then Jesus came from Galilee to the Jordan to be baptized by John. But John tried to deter him, saying, “I need to be baptized by you, and do you come to me?”<div><br></div><div>Jesus replied, “Let it be so now; it is proper for us to do this to fulfill all righteousness.” Then John consented.</div><div><br></div><div>As soon as Jesus was baptized, he went up out of the water. At that moment heaven was opened, and he saw the Spirit of God descending like a dove and alighting on him. And a voice from heaven said, “This is my Son, whom I love; with him I am well pleased.”</div><div>(Matthew 3:13–17)</div>"
+          "text": "Then Jesus came from Galilee to John at the Jordan, to be baptized by him. John would have prevented him, saying, “I need to be baptized by you, and do you come to me?” But Jesus answered him, “Let it be so now; for it is proper for us in this way to fulfill all righteousness.” Then he consented. And when Jesus had been baptized, just as he came up from the water, suddenly the heavens were opened to him and he saw the Spirit of God descending like a dove and alighting on him. And a voice from heaven said, “This is my Son, the Beloved, with whom I am well pleased.”<div>(Matthew 3:13–17)</div>"
         }
       ]
     },
@@ -99,8 +99,8 @@ var APP_DATA = {
         {
           "yaw": -0.24373764674771792,
           "pitch": -0.2789593316817669,
-          "title": "Jesus Changes Water Into Wine",
-          "text": "On the third day a wedding took place at Cana in Galilee. Jesus’ mother was there, and Jesus and his disciples had also been invited to the wedding. When the wine was gone, Jesus’ mother said to him, “They have no more wine.”<div><br></div><div>“Woman, why do you involve me?” Jesus replied. “My hour has not yet come.”</div><div><br></div><div>His mother said to the servants, “Do whatever he tells you.”</div><div><br></div><div>Nearby stood six stone water jars, the kind used by the Jews for ceremonial washing, each holding from twenty to thirty gallons.</div><div><br></div><div>Jesus said to the servants, “Fill the jars with water”; so they filled them to the brim.</div><div><br></div><div>Then he told them, “Now draw some out and take it to the master of the banquet.”</div><div><br></div><div>They did so, and the master of the banquet tasted the water that had been turned into wine. He did not realize where it had come from, though the servants who had drawn the water knew. Then he called the bridegroom aside and said, “Everyone brings out the choice wine first and then the cheaper wine after the guests have had too much to drink; but you have saved the best till now.”</div><div><br></div><div>What Jesus did here in Cana of Galilee was the first of the signs through which he revealed his glory; and his disciples believed in him.&nbsp;</div><div>(John 2:1-11)</div>"
+          "title": "The Wedding at Cana",
+          "text": "On the third day there was a wedding in Cana of Galilee, and the mother of Jesus was there. Jesus and his disciples had also been invited to the wedding. When the wine gave out, the mother of Jesus said to him, “They have no wine.” And Jesus said to her, “Woman, what concern is that to you and to me? My hour has not yet come.” His mother said to the servants, “Do whatever he tells you.” Now standing there were six stone water jars for the Jewish rites of purification, each holding twenty or thirty gallons. Jesus said to them, “Fill the jars with water.” And they filled them up to the brim. He said to them, “Now draw some out, and take it to the chief steward.” So they took it. When the steward tasted the water that had become wine, and did not know where it came from (though the servants who had drawn the water knew), the steward called the bridegroom and said to him, “Everyone serves the good wine first, and then the inferior wine after the guests have become drunk. But you have kept the good wine until now.” Jesus did this, the first of his signs, in Cana of Galilee, and revealed his glory; and his disciples believed in him.<div>(John 2:1-11)</div>"
         }
       ]
     },
@@ -154,8 +154,8 @@ var APP_DATA = {
         {
           "yaw": 0.004657148241202336,
           "pitch": -0.4721606572718109,
-          "title": "Jesus Announces the Good News",
-          "text": "After John was put in prison, Jesus went into Galilee, proclaiming the good news of God. “The time has come,\" he said. “The kingdom of God has come near. Repent and believe the good news!”&nbsp;<div>(Mark 1:14-15)</div>"
+          "title": "The Beginning of the Galilean Ministry",
+          "text": "Now after John was arrested, Jesus came into Galilee, preaching the gospel of God, and saying, “The time is fulfilled, and the kingdom of God is at hand; repent, and believe in the gospel.”<div>(Mark 1:14-15)</div>"
         }
       ]
     },
@@ -210,7 +210,7 @@ var APP_DATA = {
           "yaw": 1.71832037522912,
           "pitch": -0.1197861019002886,
           "title": "The Transfiguration",
-          "text": "After six days Jesus took with him Peter, James and John the brother of James, and led them up a high mountain by themselves. There he was transfigured before them. His face shone like the sun, and his clothes became as white as the light. Just then there appeared before them Moses and Elijah, talking with Jesus.<div><br></div><div>Peter said to Jesus, “Lord, it is good for us to be here. If you wish, I will put up three shelters—one for you, one for Moses and one for Elijah.”</div><div><br></div><div>While he was still speaking, a bright cloud covered them, and a voice from the cloud said, “This is my Son, whom I love; with him I am well pleased. Listen to him!”</div><div><br></div><div>When the disciples heard this, they fell facedown to the ground, terrified. But Jesus came and touched them. “Get up,” he said. “Don’t be afraid.” When they looked up, they saw no one except Jesus.</div><div>(Matthew 17:1-8)</div>"
+          "text": "Six days later, Jesus took with him Peter and James and his brother John and led them up a high mountain, by themselves. And he was transfigured before them, and his face shone like the sun, and his clothes became dazzling white. Suddenly there appeared to them Moses and Elijah, talking with him. Then Peter said to Jesus, “Lord, it is good for us to be here; if you wish, I will make three dwellings here, one for you, one for Moses, and one for Elijah.” While he was still speaking, suddenly a bright cloud overshadowed them, and from the cloud a voice said, “This is my Son, the Beloved; with him I am well pleased; listen to him!” When the disciples heard this, they fell to the ground and were overcome by fear. But Jesus came and touched them, saying, “Get up and do not be afraid.” And when they looked up, they saw no one except Jesus himself alone.<div>(Matthew 17:1-8)</div>"
         }
       ]
     },
@@ -248,18 +248,18 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.7996773456601955,
-          "pitch": 0.0118117757142997,
+          "yaw": 1.8911809066809484,
+          "pitch": 0.12989975817713528,
           "rotation": 0,
           "target": "3-4-the-transfiguration"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 0.06865999545901147,
-          "pitch": -0.5026090379846924,
-          "title": "The Last Supper",
-          "text": "While they were eating, Jesus took bread, and when he had given thanks, he broke it and gave it to his disciples, saying, “Take and eat; this is my body.”<div><br></div><div>Then he took a cup, and when he had given thanks, he gave it to them, saying, “Drink from it, all of you. This is my blood of the covenant, which is poured out for many for the forgiveness of sins.</div><div>(Matthew 26:26-28)</div>"
+          "yaw": 0.07802354591362004,
+          "pitch": -0.34811315586330593,
+          "title": "The Institution of the Lord’s Supper",
+          "text": "Now as they were eating, Jesus took bread, and blessed, and broke it, and gave it to the disciples and said, “Take, eat; this is my body.” And he took a cup, and when he had given thanks he gave it to them, saying, “Drink of it, all of you; for this is my blood of the] covenant, which is poured out for many for the forgiveness of sins.<div>(Matthew 26:26-28)</div>"
         }
       ]
     }
